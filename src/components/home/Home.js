61730d5b4833a0290150d7c5172135e8bcb0d08d
AnishaKarmacharya <anishakarmacharya33@gmail.com>
2022-07-12 23:07:43 +0545

    added landing section for the application

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const Home = () => {
   return (
-    <div className="preloader-wrapper absolute"><h1>Aayush</h1></div>
-  )
-}
+    <section className="home-wrapper absolute">
+      <div className="main__info">
+        <h1>Aayush</h1>
+        <p>
+          A passionate individual currently pursuing a career in product design{" "}
+          <br />
+          to utilize interperdonal design abilities and contribute sinificantly
+          to the <br />
+          company's success
+        </p>
+      </div>
+      <div className="home-socials">
+        <i class="uil uil-instagram social_icon"></i>
+        <i class="uil uil-dribbble social_icon"></i>
+        <i class="uil uil-behance social_icon"></i>
+        <i class="uil uil-linkedin-alt social_icon"></i>
+      </div>
+    </section>
+  );
+};
